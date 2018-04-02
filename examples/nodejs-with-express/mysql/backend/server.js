7be5daf05281f4a1f67f6a7556d8fbc3973584e0
shinshin86 <beagles1986@gmail.com>
2018-04-03 06:15:15 +0900

    Add example

---
+++ examples/nodejs-with-express/mysql/backend/server.js
@@ -0,0 +1,15 @@
+var express = require("express"),
+  app = express(),
+  port = process.env.PORT || 3001;
+  bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// importing route
+var routes = require('./api/routes/userRoutes');
+routes(app);
+
+app.listen(port);
+
+console.log('Sample API server started on: ' + port);
